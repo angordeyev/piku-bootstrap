2d1009cd34dc01ca1a6fd264aabd3621ade06837
angordeyev <angordeyev@gmail.com>
2021-10-10 19:47:37 +0300

    remove ansible

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,17 +18,68 @@ install -m 0600 -o "${user}" -g "${group}" "$(realpath ~root)/.ssh/authorized_ke
 apt install bc git build-essential libpcre3-dev zlib1g-dev python python3 python3-pip python3-click python3-dev python3-virtualenv python3-setuptools nginx incron acl uwsgi-core uwsgi-plugin-python3 nodeenv
 
 # Create uwgsi symlink
-test -f /usr/local/bin/uwsgi-piku || ln -s $(which uwsgi) /usr/local/bin/uwsgi-piku
+(uwgsi_symlink=/usr/local/bin/uwsgi-piku && [ -f ${uwgsi_symlink} ] || ln -s $(which uwsgi) ${uwgsi_symlink})
 
 # Install uwsgi dist script
-test -f /etc/init.d/uwsgi-piku ||
-  curl https://raw.githubusercontent.com/piku/piku/master/uwsgi-piku.dist -o /etc/init.d/uwsgi-piku &&
-  chmod 0700 /etc/init.d/uwsgi-piku
+(source=https://raw.githubusercontent.com/piku/piku/master/uwsgi-piku.dist
+dest=/etc/init.d/uwsgi-piku
+[ -f ${dest} ] || curl ${source} -o ${dest} && chmod 0700 ${dest})
 
 # Install uwsgi-piku dist script
-test -f /etc/rc2.d/S01uwsgi-piku || update-rc.d uwsgi-piku defaults
+[ -f /etc/rc2.d/S01uwsgi-piku ] || update-rc.d uwsgi-piku defaults
 
 # Install uwsgi-piku systemd script
-test -f /etc/systemd/system/uwsgi-piku.service ||
-  curl https://raw.githubusercontent.com/piku/piku/master/uwsgi-piku.service -o /etc/systemd/system/uwsgi-piku.service &&
-  chmod 0600 /etc/systemd/system/uwsgi-piku.service
+(source=https://raw.githubusercontent.com/piku/piku/master/uwsgi-piku.service
+dest=/etc/systemd/system/uwsgi-piku.service
+[ -f ${dest} ] || curl ${source} -o ${dest} && chmod 0600 ${dest})
+
+su piku <<"EOF"
+  # Fetch piku.py script
+  source=https://raw.githubusercontent.com/piku/piku/master/piku.py
+  dest=~/piku.py
+  [ -f ${dest} ] || curl ${source} -o ${dest} && chmod 0700 ${dest}
+
+  #Run piku setup
+  [ -f ~/.piku ] || python3 ~/piku.py setup
+
+  # Ask piku to use SSH keys
+  [ -f ~/.ssh/authorized_keys ] ||
+  for line in $(cat /tmp/root_authorized_keys)
+  do
+    line > /tmp/id_rsa.pub && python3 ~/piku.py setup:ssh /tmp/id_rsa.pub && rm /tmp/id_rsa.pub
+  done
+
+  # Delete root authorized keys file
+  rm /tmp/root_authorized_keys
+
+  # Download acme.sh
+  (source=https://raw.githubusercontent.com/Neilpang/acme.sh/6ff3f5d/acme.sh
+  dest=~/acme.sh
+  [ -f ${dest} ] || curl ${source} -o ${dest} && chmod 0755 ${dest})
+
+  # Execute acme.sh installer
+  [ -f ~/.acme.sh/acme.sh ] || cd ~/ && ./acme.sh --install
+
+  # Remove acme.sh installer
+  rm ~/acme.sh
+
+  # Configure acme.sh to auto-upgrade
+  sed -i 's/AUTO_UPGRADE.*/AUTO_UPGRADE=1/' ~/.acme.sh/account.conf
+EOF
+
+# Enable uwsgi-piku service
+systemctl enable uwsgi-piku
+
+# Start uwsgi init script
+systemctl start uwsgi-piku
+
+# Get nginx default config
+curl https://raw.githubusercontent.com/piku/piku/master/nginx.default.dist > /etc/nginx/sites-available/default
+
+# Restart nginx service
+systemctl restart nginx
+
+curl https://raw.githubusercontent.com/piku/piku/master/incron.dist > /etc/incron.d/piku
+
+# Restart incron service
+systemctl restart incron
